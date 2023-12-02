e7d6ae90b6a7b4f3286fdd14b130c31eb5ba74c2
Rahat Ali <rahat@rahatali.me>
2023-12-02 19:56:43 +0000

    Solution for day 1 of Advent of Code 2023

---
+++ day1.py
@@ -0,0 +1,23 @@
+# Day 1: Trebuchet?!
+
+def day1():
+    # Import text file
+    puzzle_input = open("input.txt", "r")
+    res = 0
+
+    # Itreate through each line
+    for line in puzzle_input:
+        n = []
+
+        # Add all numbers to array
+        for char in line:
+            if char.isdigit(): n.append(char)
+        
+        if len(n) == 0: continue
+        else: res += int( n[0] + n[-1] )
+        print(n, int( n[0] + n[-1] ))
+
+    return res
+
+if __name__ == "__main__":
+    print(day1())
